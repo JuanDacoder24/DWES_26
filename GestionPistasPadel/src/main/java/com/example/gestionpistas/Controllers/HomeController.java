88b899766e477fa8c59aeff331b3205fa839bc97
COMPU-S035\daw2 <servellonrejasjuandavid@gmail.com>
2025-10-23 21:33:32 +0200

    no message

--- GestionPistasPadel/src/main/java/com/example/gestionpistas/Controllers/HomeController.java
+++ GestionPistasPadel/src/main/java/com/example/gestionpistas/Controllers/HomeController.java
@@ -1,14 +1,33 @@
 package com.example.gestionpistas.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-     public class HomeController {
+import com.example.gestionpistas.Pista;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
-         @GetMapping("/")
-         public String home() {
-             return "index"; 
-         }
+
+@Controller
+    public class HomeController {
+
+    public List<Pista> listaPista = new ArrayList<Pista>();
+
+    @GetMapping("/")
+    public String mostrarPistas(Model model) {
+        return "index";
+    }
          
-     }
+    @PostMapping("/addPista")
+    public RedirectView añadirPista(@RequestParam(name="nombrePista") String nombrePista) {
+        
+
+        return new RedirectView("/");
+    }
+    
+}
--- GestionPistasPadel/src/main/java/com/example/gestionpistas/Controllers/ListasController.java
+++
@@ -1,5 +0,0 @@
-package com.example.gestionpistas.Controllers;
-
-public class ListasController {
-
-}
