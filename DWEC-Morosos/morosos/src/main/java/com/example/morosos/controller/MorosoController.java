fdbab6e0524ee28af829c3538bd9807e201eb0f8
COMPU-S035\daw2 <servellonrejasjuandavid@gmail.com>
2025-11-06 20:48:19 +0100

    no message

--- DWEC-Morosos/morosos/src/main/java/com/example/morosos/controller/MorosoController.java
+++ DWEC-Morosos/morosos/src/main/java/com/example/morosos/controller/MorosoController.java
@@ -6,17 +6,45 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.morosos.model.Moroso;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 public class MorosoController {
 
     LinkedList <Moroso> lista = new LinkedList<>();
 
-    @GetMapping("/principal")
+    @GetMapping("/api/morosos/principal")
     public LinkedList <Moroso> getMethodName() {
         return lista;
     }
+
+    @PostMapping("/api/morosos")
+    public Moroso postMethodName(@RequestBody Moroso moroso) {
+    if (moroso.getId() == null) {
+        long nuevoId = lista.isEmpty() ? 1L : lista.getLast().getId() + 1;
+        moroso.setId(nuevoId);
+    }
+    lista.add(moroso);
+    return moroso;
+    }
+    
+    @DeleteMapping("/api/morosos/{id}")
+    public String deleteMoroso(@PathVariable long id) {
+    boolean eliminado = lista.removeIf(m -> m.getId() == id);
+    if (eliminado) {
+        return "Moroso con ID " + id + " eliminado correctamente.";
+    } else {
+        return "No se encontró ningún moroso con el ID " + id;
+    }
+}
+
+    
 
     
     
